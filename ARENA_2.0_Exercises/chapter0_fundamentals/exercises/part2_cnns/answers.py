a28a8d2d7fc9acd6fc0a259372ca923378b51ce4
tran <pbtran@u.nus.edu>
2023-07-27 09:40:07 +0000

    einops and einsum

--- ARENA_2.0_Exercises/chapter0_fundamentals/exercises/part2_cnns/answers.py
+++ ARENA_2.0_Exercises/chapter0_fundamentals/exercises/part2_cnns/answers.py
@@ -7,6 +7,7 @@ import einops
 from typing import Union, Optional, Tuple
 import torch as t
 from torch import Tensor
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from jaxtyping import Float
@@ -75,7 +76,75 @@ if MAIN:
 # %%
 
 ### Exerise 7
-arr7 = einops.rearrange(arr, 'b c h w -> h (b w)')
+arr7 = einops.reduce(arr, 'b c h w -> b 1 h w', 'max')
+arr7 = einops.rearrange(arr7, 'b 1 h w -> h (b w)')
 if MAIN:
     display_array_as_img(arr7)
+# %%
+
+### Exercise 8
+arr8 = einops.reduce(arr.astype('float'), "b c h w -> h w", "min")
+if MAIN:
+    display_array_as_img(arr8)
+
+# %%
+
+### Exercise 9
+arr9 = einops.rearrange(arr[1], 'c h w -> c w h')
+if MAIN:
+    display_array_as_img(arr9)
+# %%
+
+### Exercise 10
+
+arr10 = einops.rearrange(arr, '(b1 b2) c h w -> c (b1 h) (b2 w)', b1=2, b2=3)
+arr10 = einops.reduce(arr10.astype('float'), 'c (h 2) (w 2) -> c h w', 'mean').astype('int')
+if MAIN:
+    display_array_as_img(arr10)
+# %%
+
+def einsum_trace(mat: np.ndarray):
+    '''
+    Returns the same as `np.trace`.
+    '''
+    return einops.einsum(mat, 'i i ->')
+
+def einsum_mv(mat: np.ndarray, vec: np.ndarray):
+    '''
+    Returns the same as `np.matmul`, when `mat` is a 2D array and `vec` is 1D.
+    '''
+    return einops.einsum(mat, vec, 'i k, k -> i')
+
+def einsum_mm(mat1: np.ndarray, mat2: np.ndarray):
+    '''
+    Returns the same as `np.matmul`, when `mat1` and `mat2` are both 2D arrays.
+    '''
+    return einops.einsum(mat1, mat2, 'i k, k j -> i j')
+
+def einsum_inner(vec1: np.ndarray, vec2: np.ndarray):
+    '''
+    Returns the same as `np.inner`.
+    '''
+    return einops.einsum(vec1, vec2, 'i, j ->')
+
+def einsum_outer(vec1: np.ndarray, vec2: np.ndarray):
+    '''
+    Returns the same as `np.outer`.
+    '''
+    return einops.einsum(vec1, vec2, 'i, j -> i j')
+
+
+if MAIN:
+    tests.test_einsum_trace(einsum_trace)
+    tests.test_einsum_mv(einsum_mv)
+    tests.test_einsum_mm(einsum_mm)
+    tests.test_einsum_inner(einsum_inner)
+    tests.test_einsum_outer(einsum_outer)
+
+# %%
+
+t1 = torch.tensor([1,2,3])
+t2 = torch.tensor([10,5,10])
+print(einops.einsum(t1, t2, 'i , j ->'))
+
 # %%
