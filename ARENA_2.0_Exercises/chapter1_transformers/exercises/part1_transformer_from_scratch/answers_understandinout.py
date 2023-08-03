13871f5fd4901de6d99ffa07a6b670e40b9b40a3
tran <pbtran@u.nus.edu>
2023-08-03 13:22:36 +0000

    update

---
+++ ARENA_2.0_Exercises/chapter1_transformers/exercises/part1_transformer_from_scratch/answers_understandinout.py
@@ -0,0 +1,43 @@
+#%%
+import os; os.environ['ACCELERATE_DISABLE_RICH'] = "1"
+# os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+import sys
+import einops
+from dataclasses import dataclass
+from transformer_lens import HookedTransformer
+from transformer_lens.utils import gelu_new, tokenize_and_concatenate
+import torch as t
+from torch import Tensor
+import torch.nn as nn
+import numpy as np
+import math
+from tqdm.notebook import tqdm
+from typing import Tuple, List, Optional, Dict
+from jaxtyping import Float, Int
+from transformers.models.gpt2.tokenization_gpt2_fast import GPT2TokenizerFast
+from collections import defaultdict
+from rich.table import Table
+from rich import print as rprint
+import datasets
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
+import wandb
+from pathlib import Path
+import webbrowser
+
+# Make sure exercises are in the path
+chapter = r"chapter1_transformers"
+exercises_dir = Path(f"{os.getcwd().split(chapter)[0]}/{chapter}/exercises").resolve()
+section_dir = (exercises_dir / "part1_transformer_from_scratch").resolve()
+if str(exercises_dir) not in sys.path: sys.path.append(str(exercises_dir))
+
+from plotly_utils import imshow
+# import part1_transformer_from_scratch.solutions as solutions
+
+device = t.device("cuda" if t.cuda.is_available() else "cpu")
+
+MAIN = __name__ == '__main__'
+
+reference_gpt2 = HookedTransformer.from_pretrained("gpt2-small", fold_ln=False, center_unembed=False, center_writing_weights=False)
+# %%
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# sota-wcode-dl
+# Studying Deep Learning
+
+### A repository to study about deep learning
+
+#### 1. Prerequisite
+
+1/ Study numpy-100 where all exercises are good to know
+
+2/ Study tensor-puzzles to know about tensors
+
+3/ Einsum and einops which are essentials for matrix manipulation
+
+#### 2. Modules
+
+1/ ARENA_2.0: A really good intro into transformer. It teaches you about everything you want to study about vision, reinforcement learning, scale and is really good in general.
